75b884f8fbe4da7fdd7c2e6f13db6605cd637fd6
Marcos Tonina <marcos.tonina@outlook.com>
2024-06-27 16:39:14 -0300

    Adding error handling

---
+++ src/main/java/com/marcostonina/nonconstructiblechange/web/CurrencyControllerAdvice.java
@@ -0,0 +1,26 @@
+package com.marcostonina.nonconstructiblechange.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+
+@RestControllerAdvice
+public class CurrencyControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    HashMap<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        var errors = new HashMap<String, String>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+}
