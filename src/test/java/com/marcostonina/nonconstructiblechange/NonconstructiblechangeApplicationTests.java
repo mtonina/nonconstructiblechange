0b8dac2e90396ad2ca0a59d414f12c2eb0833800
Marcos Tonina <marcos.tonina@outlook.com>
2024-07-06 17:35:58 -0300

    Adding integration test for get currencies

--- src/test/java/com/marcostonina/nonconstructiblechange/NonconstructiblechangeApplicationTests.java
+++ src/test/java/com/marcostonina/nonconstructiblechange/NonconstructiblechangeApplicationTests.java
@@ -9,6 +9,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -40,6 +41,39 @@ class NonconstructiblechangeApplicationTests {
 					assertThat(actualCurrency.name()).isEqualTo(currency.name());
 					assertThat(actualCurrency.code()).isEqualTo(currency.code());
 					assertThat(actualCurrency.coins()).hasSameElementsAs(currency.coins());
+				});
+
+	}
+
+	@Test
+	void whenGetRequestWithCurrencyCreatedThenCurrencyReturned() {
+		var currency = new Currency("Dollar", "USD", Arrays.asList(1,2,3));
+
+		webClient
+				.post()
+				.uri("currencies")
+				.bodyValue(currency)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody(Currency.class).value(actualCurrency -> {
+					assertThat(actualCurrency).isNotNull();
+					assertThat(actualCurrency.name()).isEqualTo(currency.name());
+					assertThat(actualCurrency.code()).isEqualTo(currency.code());
+					assertThat(actualCurrency.coins()).hasSameElementsAs(currency.coins());
+				});
+		webClient
+				.get()
+				.uri("currencies")
+				.exchange()
+				.expectStatus().isOk()
+				.expectBodyList(Currency.class).value(currencyList -> {
+					assertThat(currencyList).isNotNull();
+					assertThat(currencyList).hasSize(1);
+					currencyList.forEach(actualCurrency -> {
+						assertThat(actualCurrency.name()).isEqualTo(currency.name());
+						assertThat(actualCurrency.code()).isEqualTo(currency.code());
+						assertThat(actualCurrency.coins()).hasSameElementsAs(currency.coins());
+					});
 				});
 
 	}
